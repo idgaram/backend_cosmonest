b0b68cf6ec1d764c5f0726b31b58fda0374cb4b5
Lorys Lellouche <lorys.lellouche@gmail.com>
2024-07-18 15:51:11 +0200

    avec bonjour user

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const data = require("./data");
+const port = 3310;
 
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
@@ -23,6 +24,20 @@ mongoose.connect("mongodb://localhost:27017/cosmonest", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// Middleware d'authentification
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, "your_jwt_secret", (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -52,6 +67,18 @@ app.post("/register", async (req, res) => {
   res.status(201).send("User registered successfully");
 });
 
+app.get("/api/profile", authenticateToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json({ username: user.username, email: user.email });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user data" });
+  }
+});
+
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
@@ -69,11 +96,13 @@ app.post("/login", async (req, res) => {
     return res.status(400).send("Invalid credentials");
   }
 
+  const username = user.username;
+
   const token = jwt.sign({ userId: user._id }, "your_jwt_secret", {
     expiresIn: "1h",
   });
 
-  res.status(200).json({ token });
+  res.status(200).json({ token, username });
 });
 
 app.get("/", sayData);
@@ -91,8 +120,6 @@ app.get("/destinationchoisie/:id", (req, res) => {
   const destination = data.find((elem) => elem.Id === destinationId);
   res.json(destination);
 });
-
-const port = 3310;
 
 app.listen(port, () => {
   console.log(`example app listening on port ${port}`);
